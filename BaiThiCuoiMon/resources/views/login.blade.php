56e409ae5708445d6552ec9969b2ab7af2916641
VTNQ <votannamquoc@gmail.com>
2023-05-19 19:47:01 +0700

    up

--- BaiThiCuoiMon/resources/views/login.blade.php
+++ BaiThiCuoiMon/resources/views/login.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <title>Untitled</title>
-    <link rel="stylesheet" href="{{assets('bootstrap/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
 </head>
 
-<body>
+<body style="background: url(http://127.0.0.1:8000/images/background-login.webp);background-repeat: no-repeat;background-size: cover;">
     <section class="py-4 py-xl-5">
         <div class="container">
             <div class="row mb-5">
@@ -17,13 +17,13 @@
                 </div>
             </div>
             <div class="row d-flex justify-content-center">
-                <div class="col-md-6 col-xl-4">
+                <div class="col-md-6 col-xl-4" style="width: 16vw">
                     <div class="card mb-5">
                         <div class="card-body d-flex flex-column align-items-center">
-                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"><svg class="bi bi-person" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
-                                </svg></div>
-                            <form class="text-center" method="post">
+                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4">
+                              <i class="fa-solid fa-user fa-2xl"></i>
+                            </div>
+                            <form class="text-center" method="post" action="" style="margin: 6vh auto 0">
                                 <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email" /></div>
                                 <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password" /></div>
                                 <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
@@ -35,6 +35,8 @@
             </div>
         </div>
     </section>
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="https://kit.fontawesome.com/b6daa7a43d.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
