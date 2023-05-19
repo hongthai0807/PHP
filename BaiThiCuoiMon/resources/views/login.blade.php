6e5b3fcb7189e86e71eab16a40636b2ab7996c2e
hongthai0807 <hongthai0807@gmail.com>
2023-05-19 19:50:28 +0700

    a

--- BaiThiCuoiMon/resources/views/login.blade.php
+++ BaiThiCuoiMon/resources/views/login.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORM LOGIN</title>
+    <link rel="stylesheet" href="..css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <link rel="stylesheet" href="../css/login.css">
     <form class="form">
     <div class="title">Welcome to Devsne,<br><span>please sign up to continue</span></div>
     <input type="email" placeholder="Email" name="email" class="input">
