18e242f7e5d773b40cfe02bac689d0390deb0c37
VTNQ <votannamquoc@gmail.com>
2023-05-19 19:09:36 +0700

    up

--- BaiThiCuoiMon/resources/views/login.blade.php
+++ BaiThiCuoiMon/resources/views/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <title>Untitled</title>
+    <link rel="stylesheet" href="{{assets('bootstrap/bootstrap.css')}}">
 </head>
 
 <body>
