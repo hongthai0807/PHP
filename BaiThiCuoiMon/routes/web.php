965956fade271471b31d4f9a92f0df5e82ed548d
VTNQ <votannamquoc@gmail.com>
2023-05-22 17:27:28 +0700

    update

--- BaiThiCuoiMon/routes/web.php
+++ BaiThiCuoiMon/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
  Route::get('/', function () {
-      return view('welcome');
+      return view('login');
   });
 Route::get('/admin',[App\Http\Controllers\FeedbackController::class,'admin'])->name('admin');
 Route::post('/user',[FeedbackController::class,'checksession'])->name('user');
