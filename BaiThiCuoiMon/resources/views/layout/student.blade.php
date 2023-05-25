7eee09fef4277c8ad0b757d0038bcdc7c04312c6
hongthai0807 <hongthai0807@gmail.com>
2023-05-25 08:44:58 +0700

    a

--- BaiThiCuoiMon/resources/views/layout/student.blade.php
+++ BaiThiCuoiMon/resources/views/layout/student.blade.php
@@ -87,10 +87,7 @@
             </div>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-              <i class="ti-bell mx-0"></i>
-              <span class="count"></span>
-            </a>
+
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="notificationDropdown">
               <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
               <a class="dropdown-item">
